67660e7d554a98e1c5e3eaba1607ac3e5221da9c
Hanif Warren <hanif.warren@gmail.com>
2018-05-10 16:26:04 -0400

    Fixed error where MainCtrl function was incomplete (it was example code). App saves and edits contacts

--- HWarrenContactsApp/Scripts/Client/MainCtrl.js
+++ HWarrenContactsApp/Scripts/Client/MainCtrl.js
@@ -1,21 +1,25 @@
 ﻿(function (app) {
 
+//    var MainCtrl = function ($scope, $http) {
+//  $scope.loading = true;
+//  $http.post('/endpoint', { foo: 'bar' }).success(function(response) {
+//    $scope.response = response;
+//    $scope.loading = false;
+//  });
+
+
+//};
+
+
     var MainCtrl = function ($scope, $http) {
   $scope.loading = true;
-  $http.post('/endpoint', { foo: 'bar' }).success(function(response) {
+  $http.post((contactsApiUrl, PhoneRecord)).success(function(response) {
     $scope.response = response;
     $scope.loading = false;
   });
 
-        $http.post((contactsApiUrl, PhoneRecord)).success(function(response) {
-    $scope.response = response;
-    $scope.loading = false;
-  });
 
-        (contactsApiUrl, PhoneRecord)
 };
-
-
 
 
 
