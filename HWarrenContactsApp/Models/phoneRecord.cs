9d912cdaabcad0da8eb9d7ced8976a22843dff4d
Hanif Warren <hanif.warren@gmail.com>
2018-05-09 20:08:13 -0400

    Set up initial models for contacts

---
+++ HWarrenContactsApp/Models/phoneRecord.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HWarrenContactsApp.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class PhoneRecord
+    {
+        public PhoneRecord()
+        {
+            this.relationshipType = new HashSet<relationshipType>();
+        }
+
+
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        public int Phone { get; set; }
+        public DateTimeOffset Birthdate { get; set; }
+        public string MediaUrl { get; set; }
+
+
+
+        }
+    }
+}
---
+++ HWarrenContactsApp/Models/relationshipType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HWarrenContactsApp.Models
+{
+    public class  relationshipType
+
+    {
+        public int Id { get; set; }
+
+        public string relationshipName { get; set; }
+    }
+}
