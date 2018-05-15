83660816535e180eb60879750fb9ada29868d2ac
Hanif Warren <hanif.warren@gmail.com>
2018-05-15 18:14:09 -0400

    Installed tinymce nuget package, added comments field and edited the id tinymce not working yet (although the field saves it)

--- HWarrenContactsApp/Models/phoneRecord.cs
+++ HWarrenContactsApp/Models/phoneRecord.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.ComponentModel.DataAnnotations;
-    using System.Data.Entity;
+using System.Data.Entity;
 
 namespace HWarrenContactsApp.Models
 {
+    using System.Web.Mvc;
 
     public class PhoneRecord
     {
@@ -29,6 +30,7 @@ namespace HWarrenContactsApp.Models
         [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}", ApplyFormatInEditMode = true)]
         public DateTimeOffset? Birthdate { get; set; }
         public string MediaUrl { get; set; }
+        [AllowHtml]
         public string Comments { get; set; }
 
         public virtual ICollection<relationshipType> relationshipTypes { get; set; }
