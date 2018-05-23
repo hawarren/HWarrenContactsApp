01f9ddd225404da4aa894178ec385d6cd4a2b967
Hanif Warren <hanif.warren@gmail.com>
2018-05-23 01:16:12 -0400

    Seeded a few more contacts, don't need to assign them Id properties because those are done automatically.

--- HWarrenContactsApp/Migrations/Configuration.cs
+++ HWarrenContactsApp/Migrations/Configuration.cs
@@ -30,11 +30,42 @@ namespace HWarrenContactsApp.Migrations
             //
             context.PhoneRecords.Add(new PhoneRecord
                                          {
-                                             Id = 1,
                                              FirstName = "Seedy1",
                                              LastName = "McSeedy1",
                                              Email = "Seedy1@McSeedy1@gmail.com"
                                          });
+            context.PhoneRecords.Add(new PhoneRecord
+            {
+                FirstName = "Seedy2",
+                LastName = "McSeedy2",
+                Email = "Seedy2@McSeedy2@gmail.com"
+            });
+            context.PhoneRecords.Add(new PhoneRecord
+            {
+                FirstName = "Seedy3",
+                LastName = "McSeedy3",
+                Email = "Seedy3@McSeedy1@gmail.com"
+            });
+            context.PhoneRecords.Add(new PhoneRecord
+            {
+                FirstName = "Seedy4",
+                LastName = "McSeedy4",
+                Email = "Seedy4@McSeedy4@gmail.com"
+            });
+            context.PhoneRecords.Add(new PhoneRecord
+            {
+                FirstName = "Seedy5",
+                LastName = "McSeedy5",
+                Email = "Seedy5@McSeedy5@gmail.com"
+            });
+            context.PhoneRecords.Add(new PhoneRecord
+            {
+
+                FirstName = "Seedy6",
+                LastName = "McSeedy6",
+                Email = "Seedy6@McSeedy6@gmail.com"
+            });
+
         }
     }
 }
