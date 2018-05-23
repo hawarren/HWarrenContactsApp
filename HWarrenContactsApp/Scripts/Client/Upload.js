47018320f0cbc80aec0049ae878729e783850294
Hanif Warren <hanif.warren@gmail.com>
2018-05-23 02:44:30 -0400

    Fully implement standalone upload image functionality. Need to fix bug where button doesn't respond, yet image is still sent and saved to the DB (also maybe check for duplicates in controller) Also need to modify scripts and view to do it in angularjs view

---
+++ HWarrenContactsApp/Scripts/Client/Upload.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function() {
+    $('#btnUploadFile').
+    on('click', function() {
+        var data = new FormData()
+        var files = $("#fileUpload").get(0).files;
+        // Add the uploaded image content to the form data collection
+        if (files.length > 0) {
+            data.append("UploadedImage", files[0]);
+        }
+        //Ajax request with the contentType = false, and processDate = false
+        var ajaxRequest = $.ajax({
+            type: "POST",
+            //make sure this uri is correct
+            url: "http://localhost:51072/api/FileUploads",
+            contentType: false,
+            processData: false,
+            data: data
+        });
+    });
+});
